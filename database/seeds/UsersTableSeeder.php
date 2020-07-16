1ebb10d27469fd0ee5411e69965391739894dfb6
Gerson Sousa <gersonsis2018@gmail.com>
2020-07-15 22:18:31 -0300

    1.0 upload de imagens salvando.

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Administrador do Sistema',
+            'idade' => 40,
+            'contato' => '096XXXXXXXXX',
+            'email' => 'administrador@gmail.com',
+            'password' => bcrypt('admin@123'),
+            'status' => 'administrador'
+        ]);
+    }
+}
